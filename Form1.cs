90003e60dd5c44457b12b89eabdc7151df05b8e7
Paul Moore <bossbossbosscow@gmail.com>
2021-03-16 12:51:58 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class frmMain : Form
+    {
+        TileLabel[,] tiles = new TileLabel[9, 9];         
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tile_MouseEnter(object sender, System.EventArgs e)
+        {
+            var tile = (TileLabel)sender;
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    if (tiles[i, j] == tile)
+                    {
+                        for (int k = 0; k < 9; k++)
+                        {
+                            tiles[i, k].BackColor = Color.Yellow;
+                            tiles[k, j].BackColor = Color.Yellow;
+                        }
+                        for (int l = 0; l < 9; l++)
+                            for (int m = 0; m < 9; m++)
+                            {
+                                if (l / 3 == i / 3 & m / 3 == j / 3)
+                                    tiles[l, m].BackColor = Color.Yellow;
+                            }
+                    }
+                }
+            
+        }
+        private void tile_MouseLeave(object sender, System.EventArgs e)
+        {
+            var tile = (TileLabel)sender;
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    if (tiles[i, j] == tile)
+                    {
+                        for (int k = 0; k < 9; k++)
+                        {
+                            tiles[i, k].BackColor = Color.White;
+                            tiles[k, j].BackColor = Color.White;
+                        }
+                        for (int l = 0; l < 9; l++)
+                            for (int m = 0; m < 9; m++)
+                            {
+                                if (l / 3 == i / 3 & m / 3 == j / 3)
+                                    tiles[l, m].BackColor = Color.White;
+                            }
+                    }
+                }
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    tiles[i, j] = new TileLabel(i, j, this);
+                    this.Controls.Add(tiles[i, j]);
+                    tiles[i, j].MouseEnter += new EventHandler(tile_MouseEnter);
+                    tiles[i, j].MouseLeave += new EventHandler(tile_MouseLeave);
+                }
+        }
+    }
+}
---
+++ TileLabel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    class TileLabel: Label
+    {
+        private bool[] hintsBool;
+        public Label[] hintsLabel;
+        private bool selected;
+        public TileLabel(int x, int y, Form board) : base()
+        {            
+            this.Location = new Point(5 + (55 * x), 5 + (55 * y));
+            this.Size = new Size(50, 50);
+            selected = false;
+            hintsBool = new bool[9];
+            hintsLabel = new Label[9];            
+            for (int i = 0; i < 9; i++)
+            {
+                hintsLabel[i] = new Label();
+                board.Controls.Add(hintsLabel[i]);
+                hintsLabel[i].Location = new Point((i % 3) * 17 + this.Location.X + 5
+                    , (i / 3) * 17 + this.Location.Y + 5);
+                hintsLabel[i].BackColor = Color.Red;
+                hintsLabel[i].Size = new Size(5, 5);
+                hintsLabel[i].Visible = false;
+            }                       
+        }
+    }
+}
